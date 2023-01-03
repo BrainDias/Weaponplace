cff8da4f6eb8dd904a14cc66ad1abd4b68039806
BrainDias <yankee.democratic@gmail.com>
2023-01-03 08:15:36 +0300

    Created JUnit Test + separated Controller

--- src/main/java/com/example/demo/controller/greetingController.java
+++ src/main/java/com/example/demo/controller/greetingController.java
@@ -1,21 +1,14 @@
 package com.example.demo.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
-public class greetingController {
-
-	public static void main(String[] args) {
-		SpringApplication.run(greetingController.class, args);
-	}
+public class GreetingController {
 
 	@GetMapping("/greeting")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public static String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "Hello " + name + '!';
 	}
 }

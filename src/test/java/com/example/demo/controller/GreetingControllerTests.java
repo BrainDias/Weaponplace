7b42c016546549b6cbf784715a3bf451174441aa
BrainDias <yankee.democratic@gmail.com>
2023-04-16 14:58:57 +0300

    Mocking fields by annotations with MockitoExtension for test class

--- src/test/java/com/example/demo/controller/GreetingControllerTests.java
+++ src/test/java/com/example/demo/controller/GreetingControllerTests.java
@@ -1,21 +1,25 @@
 package com.example.demo.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.example.demo.services.GreetingService;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class GreetingControllerTests {
-
-@InjectMocks
-GreetingService serviceMock;
+	@Mock
+	private GreetingService serviceMock;
+	@InjectMocks
+	private GreetingController greetingControllerWithMocks;
 	@Test
 	void greetingEndpoint() {
 		String name = "Mary";
-		Mockito.when(serviceMock.printHello(name)).thenReturn("Hello "+name+"!");
-		assertEquals(new GreetingController(serviceMock).hello(name), "Hello " + name + "!");
+		Mockito.when(serviceMock.printHello(name)).thenReturn("Hello " + name + "!");
+		assertEquals(greetingControllerWithMocks.hello(name), "Hello " + name + "!");
 	}
 }
